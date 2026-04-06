832b4b565a5e38842d8b16f037d6c7597f1c8c4a
Peyton <hpeyton33@gmail.com>
2026-04-06 07:08:07 -0500

    fix tests

--- src/tests/home.test.tsx
+++ src/tests/home.test.tsx
@@ -3,13 +3,13 @@ import { filterFavorites } from "../lib/favorites"
 
 describe("favorites should never be larger than 3", () => {
   it("limits favorites to 3", () => {
-    const testProducts: MyProduct[] = [
-      { id: 0, name: "test1", description: "test1", price: 0, numberInCart: 0, favorite: true, image:'x'},
-      { id: 1, name: "test2", description: "test2", price: 0, numberInCart: 0, favorite: false, image:'x'},
-      { id: 2, name: "test3", description: "test3", price: 0, numberInCart: 0, favorite: true, image:'x'},
-      { id: 3, name: "test4", description: "test4", price: 0, numberInCart: 0, favorite: true, image:'x'},
-      { id: 4, name: "test5", description: "test5", price: 0, numberInCart: 0, favorite: false, image:'x'},
-      { id: 5, name: "test6", description: "test6", price: 0, numberInCart: 0, favorite: true, image:'x'}
+   const testProducts: MyProduct[] = [
+      { id: 0, name: "test1", description: "test1", price: 0, numberInCart: 0, favorite: true, image:'x',catagory:""},
+      { id: 1, name: "test2", description: "test2", price: 0, numberInCart: 0, favorite: true, image:'x',catagory:""},
+      { id: 2, name: "test3", description: "test3", price: 0, numberInCart: 0, favorite: false, image:'x',catagory:""},
+      { id: 3, name: "test4", description: "test4", price: 0, numberInCart: 0, favorite: false, image:'x',catagory:""},
+      { id: 4, name: "test5", description: "test5", price: 0, numberInCart: 0, favorite: false, image:'x',catagory:""},
+      { id: 5, name: "test6", description: "test6", price: 0, numberInCart: 0, favorite: false, image:'x',catagory:""}
     ];
 
     expect(filterFavorites(testProducts).length).toBeLessThanOrEqual(3);
