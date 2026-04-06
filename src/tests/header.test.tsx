29b2f0df7f362bf0dffc81edc70bc018915b1811
Peyton <hpeyton33@gmail.com>
2026-04-06 07:06:47 -0500

    editted tests to include missing elements

--- src/tests/header.test.tsx
+++ src/tests/header.test.tsx
@@ -11,12 +11,12 @@ import { __resetCartForTests } from "../hooks/usecart";
    
    
    const testProducts: MyProduct[] = [
-      { id: 0, name: "test1", description: "test1", price: 0, numberInCart: 0, favorite: true, image:'x'},
-      { id: 1, name: "test2", description: "test2", price: 0, numberInCart: 0, favorite: true, image:'x'},
-      { id: 2, name: "test3", description: "test3", price: 0, numberInCart: 0, favorite: false, image:'x'},
-      { id: 3, name: "test4", description: "test4", price: 0, numberInCart: 0, favorite: false, image:'x'},
-      { id: 4, name: "test5", description: "test5", price: 0, numberInCart: 0, favorite: false, image:'x'},
-      { id: 5, name: "test6", description: "test6", price: 0, numberInCart: 0, favorite: false, image:'x'}
+      { id: 0, name: "test1", description: "test1", price: 0, numberInCart: 0, favorite: true, image:'x',catagory:""},
+      { id: 1, name: "test2", description: "test2", price: 0, numberInCart: 0, favorite: true, image:'x',catagory:""},
+      { id: 2, name: "test3", description: "test3", price: 0, numberInCart: 0, favorite: false, image:'x',catagory:""},
+      { id: 3, name: "test4", description: "test4", price: 0, numberInCart: 0, favorite: false, image:'x',catagory:""},
+      { id: 4, name: "test5", description: "test5", price: 0, numberInCart: 0, favorite: false, image:'x',catagory:""},
+      { id: 5, name: "test6", description: "test6", price: 0, numberInCart: 0, favorite: false, image:'x',catagory:""}
     ];
 
  beforeEach(()=>{
